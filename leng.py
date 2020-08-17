fe58fda121c0823157222ce8ac05d1a5c9645ff9
ChenAa110 <540020464@qq.com>
2020-08-17 21:47:27 +0800

    My first commit`

---
+++ leng.py
@@ -0,0 +1,19 @@
+import math
+
+l = 4
+w = 10
+d = math.sqrt(l ** 2 + w ** 2)
+
+print(d)
+
+print(3**3)
+print(8//2)
+print(10//2)
+print(10%3)
+print(10%2)
+
+print(not 1==1)
+print(not 1==2)
+
+
+acac
